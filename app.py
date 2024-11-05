1e0d5517395972871c6f125f9678f479a4b84c52
liang <ccl.cis87@gmail.com>
2024-11-05 15:09:44 +0800

    add bottle

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("start")
+from bottle import get, post, run, request
+import sys
+
+@get('/')
+def index():
+    return "This is api page for processing POSTed messages"
+
+@post('/')
+def index():
+    print(request.body.getvalue().decode('utf-8'), file=sys.stdout)
+    return request.body
+
+run(host='localhost', port=8082, debug=True)
