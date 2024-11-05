cfa65d93c1c5045c271323b16edf58c233288f73
liang <ccl.cis87@gmail.com>
2024-11-05 16:02:25 +0800

    modified binding --1

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def index():
 def index():
     print(request.body.getvalue().decode('utf-8'), file=sys.stdout)
     return request.body
-
-run(host='localhost', port=10000, debug=True)
+myport= (10000 || 4000)
+run(host='0.0.0.0', port = myport, debug=True)
