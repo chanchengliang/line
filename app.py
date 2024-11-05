ad8a97fc8fef1adb070ad5354ce799072237503c
liang <ccl.cis87@gmail.com>
2024-11-05 17:13:52 +0800

    add _stderr to log

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import sys
 
 @get('/')
 def index():
-    print("/index")
+    _stderr("/index")
     return "This is api page for processing POSTed messages"
 
 @post('/')
 def index():
-    print(request.body.getvalue().decode('utf-8'))
+    _stderr(request.body.getvalue().decode('utf-8'))
     return request.body
 myport= 10000 
 run(host='0.0.0.0', port = myport, debug=True)
