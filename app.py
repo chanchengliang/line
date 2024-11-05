d41b99a8d02aae9ec217a75737e24a09bfcbcf88
liang <ccl.cis87@gmail.com>
2024-11-05 16:31:28 +0800

    change print to default

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import sys
 
 @get('/')
 def index():
+    print("/index")
     return "This is api page for processing POSTed messages"
 
 @post('/')
 def index():
-    print(request.body.getvalue().decode('utf-8'), file=sys.stdout)
+    print(request.body.getvalue().decode('utf-8'))
     return request.body
 myport= 10000 
 run(host='0.0.0.0', port = myport, debug=True)
